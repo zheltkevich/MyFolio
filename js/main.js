83a09166d108b01d506bd2afba3509970d494286
Aleksandr <36589738+sugar42@users.noreply.github.com>
2020-11-06 11:58:24 +0300

    v0.09

--- js/main.js
+++ js/main.js
@@ -3,5 +3,6 @@ let burgerButton = document.querySelector('.burger-button')
 
 burgerButton.onclick = function () {
   navbar.classList.toggle('main-header__side-navbar--open');
+  document.querySelector('.body').classList.toggle('lock');
   console.log('Кнопка меню нажата');
 };
